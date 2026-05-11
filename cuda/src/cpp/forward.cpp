bf65a17ec909742ac8c749c3d3a4e32a5ff30c7e
Billy Moses <wmoses@google.com>
2026-05-11 12:43:37 -0500

    Keep only first loop in process_src_locations

--- cuda/src/cpp/forward.cpp
+++ cuda/src/cpp/forward.cpp
@@ -12,8 +12,6 @@ void Class_Forward_Specs::process_src_locations ( struct_src_input & src_input )
     constexpr int N_dir = ns_forward::N_dir;
 
     std::array<char, N_dir> & grid_type = src_input.src_grid_type;
-    // Comment out map access
-    // Class_Grid * grid_src = Map_Grid_pointers.at(grid_type);
 
     std::array<long, N_dir> num_location {}; num_location.fill(-1);
     std::array<long, N_dir> den_location {}; den_location.fill(-1);
@@ -22,39 +20,6 @@ void Class_Forward_Specs::process_src_locations ( struct_src_input & src_input )
         num_location[i_dir] = src_input.src_location[i_dir*2  ];
         den_location[i_dir] = src_input.src_location[i_dir*2+1];    
     }
-
-    std::array<long, N_dir> index_src {}; index_src.fill(-1);
-
-    for ( int i_dir = 0; i_dir < N_dir; i_dir++ )
-    {
-        if ( grid_type[i_dir] == 'N' )
-        {
-            long num_src = num_location[i_dir] * ns_input::den_dx_soln;
-            long den_src = den_location[i_dir] * ns_input::num_dx_soln;
-            index_src[i_dir] = num_src / den_src;
-        }
-        else if ( grid_type[i_dir] == 'M' )
-        {
-            long num_src = 2 * num_location[i_dir] * ns_input::den_dx_soln - den_location[i_dir] * ns_input::num_dx_soln;
-            long den_src = 2 * den_location[i_dir] * ns_input::num_dx_soln;
-            index_src[i_dir] = num_src / den_src;
-        }
-    }
-    
-    src_forward.is_src = true;
-    src_forward.src_index = src_input.src_index;
-    src_forward.grid_type = src_input.src_grid_type;
-    src_forward.ix_src = index_src[0] - 0; // Hardcoded
-    src_forward.iy_src = index_src[1] - 0; // Hardcoded
-    src_forward.I_SRC  = src_forward.ix_src * 600 + src_forward.iy_src; // Hardcoded size_y=600
-    src_forward.c_source_type = 'V'; // Hardcoded
-
-    printf( "\n ---- Found source of type %c at (%3d %3d - %d) on grid (%c %c) of sizes (%3d %3d)\n\n", 
-            src_forward.c_source_type,
-            src_forward.ix_src, src_forward.iy_src, src_forward.I_SRC, 
-            'N', 'M', // Hardcoded grid types
-            601, 600 ); // Hardcoded sizes
-    fflush(stdout);
 }
 
 void Class_Forward_Specs::process_rcv_locations ( std::vector< struct_rcv_input > & Vec_Rcv_Input ) 
