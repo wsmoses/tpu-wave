534386f86ec5334ef2c4c09f160be574d125191e
William S. Moses <gh@wsmoses.com>
2026-05-11 17:29:30 -0500

    true mwe

--- cuda/src/cuda_main/nvcc_device_FDM_2D.cu
+++ cuda/src/cuda_main/nvcc_device_FDM_2D.cu
@@ -1,29 +1,32 @@
-#include "forward.hpp"
-#include "inverse.hpp"
 
-#include "grid.cuh"
-#include "namespace_device_variable.cuh"  /* probably dummy, only the #include "namespace_type.cuh" inside it is meaningful */
+#include <stdio.h>
+#include <array>
+#include <map>
+#include <vector>
+    // constexpr auto define_Array_Grid_types ()
+    consteval auto define_Array_Grid_types ()    // if some compiler versions complain,
+    {                                            // try changing consteval to constexpr
+        std::array< std::array<char, 2>, 4 > Array_Grid_types {};
+                                                             Array_Grid_types.at(0) = {'N','N'};
+                                                             Array_Grid_types.at(1) = {'M','N'};
+                                                             Array_Grid_types.at(2) = {'N','M'};
+                                                             Array_Grid_types.at(3) = {'M','M'};
+        return Array_Grid_types;
+    }
 
 int main(int argc, char* argv[]) 
 {
-    Class_Forward_Specs Fwd_Specs;
-    Class_Inverse_Specs Inv_Specs;
-    
-    constexpr auto Array_Grid_types = ns_forward::define_Array_Grid_types ();
-    std::map< int , std::vector< struct_rcv_input > > & Map_Vec_Rcv_Input = Inv_Specs.Map_Vec_Rcv_Input;
+   
+           std::map< std::array<char,2> , std::vector<double> > Map_grid_record_rcv;  // involve memory
 
-    struct_src_input iter_vec { 1 , {'N','N'} , {0,1,0,1} };
-    Map_Vec_Rcv_Input[1] = { struct_rcv_input { 1 , 1 , {'N','N'} , {0,1,0,1} } };
-
-    //Inv_Specs.data_misfit = 0.;  // (re)set the aggregated data misfit for all sources to zero before 
-                                 // enter the loop that goes through the sources and lauches simulations 
-
+    constexpr auto Array_Grid_types = define_Array_Grid_types ();
     for (int i=0; i<1; i++) {
-        int fwd_src_index = 1;//Fwd_Specs.src_forward.src_index;
 
-        for ( const auto & grid_type : Array_Grid_types ) { Fwd_Specs.Map_grid_record_rcv        [grid_type] = {}; }
+        for ( const auto & grid_type : Array_Grid_types ) { 
+		Map_grid_record_rcv        [grid_type] = {};
+	  printf("saw grid size %c %c\n", grid_type[0], grid_type[1]);
+	}
 
-        Fwd_Specs.process_rcv_locations ( Map_Vec_Rcv_Input.at( fwd_src_index ) );
     }
 
     return 0;
