29ad08e20e35fe3f273b1956413f56def0a8c5e9
Billy Moses <wmoses@google.com>
2026-05-13 13:19:11 -0500

    inline

--- cuda/src/cuda_main/nvcc_device_FDM_2D.cu
+++ cuda/src/cuda_main/nvcc_device_FDM_2D.cu
@@ -27,8 +27,35 @@ int main(int argc, char* argv[])
     {
 
 
-		grid_SMM.energy_calculation ();
-		grid_SNN.energy_calculation ();
+        // Inlined energy_calculation for grid_SMM
+        {
+            auto & Sxx = grid_SMM.Vec_soln[0];
+            auto & Syy = grid_SMM.Vec_soln[1];
+            auto & P1  = grid_SMM.Vec_prmt_enrg[0];
+            auto & P2  = grid_SMM.Vec_prmt_enrg[1];
+            auto & T   = grid_SMM.thrust_memory;
+
+            weighted_square_NORMAL_grid <cuda_Struct_Grid<'M', 'M', 600, 600>> <<< 600 , 32 >>> ( Sxx.ptr , Syy.ptr , P1.ptr , P2.ptr , T.ptr );
+
+            double * d_result = nullptr;
+            cudaMalloc( &d_result , sizeof(double) );
+            single_thread_reduce <cuda_Struct_Grid<'M', 'M', 600, 600>::length_memory> <<< 1 , 1 >>> ( T.ptr , d_result );
+            cudaFree( d_result );
+        }
+
+        // Inlined energy_calculation for grid_SNN
+        {
+            auto & S = grid_SNN.Vec_soln[0];
+            auto & P = grid_SNN.Vec_prmt_enrg[0];
+            auto & T = grid_SNN.thrust_memory;
+
+            weighted_square_SINGLE_grid <cuda_Struct_Grid<'N', 'N', 601, 601>> <<< 601 , 32 >>> ( S.ptr , P.ptr , T.ptr );
+
+            double * d_result = nullptr;
+            cudaMalloc( &d_result , sizeof(double) );
+            single_thread_reduce <cuda_Struct_Grid<'N', 'N', 601, 601>::length_memory> <<< 1 , 1 >>> ( T.ptr , d_result );
+            cudaFree( d_result );
+        }
 
     }  // for (int it=0; it<Nt; it++) 
 
