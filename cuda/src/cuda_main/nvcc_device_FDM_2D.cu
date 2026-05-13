fa8604afdd1fa7d5ae42ca3fbc346754811dc107
William S. Moses <gh@wsmoses.com>
2026-05-12 23:36:58 -0500

    more clean

--- cuda/src/cuda_main/nvcc_device_FDM_2D.cu
+++ cuda/src/cuda_main/nvcc_device_FDM_2D.cu
@@ -1,20 +1,12 @@
-#include "forward.hpp"
-#include "inverse.hpp"
-
 #include "input_processing.hpp"
 
 #include "grid.cuh"
 #include "namespace_device_variable.cuh"  /* probably dummy, only the #include "namespace_type.cuh" inside it is meaningful */
 
-#include <filesystem>
-
 int main(int argc, char* argv[]) 
 {
     using ns_forward::N_dir;
     using namespace ns_input;
-
-    Class_Forward_Specs Fwd_Specs;
-    Class_Inverse_Specs Inv_Specs;
 
 #ifndef GRIDFLAG
     std::string input_file_name = "/InputFile.txt";
@@ -40,61 +32,32 @@ int main(int argc, char* argv[])
     // ---- rescanning the command line options to process the remaining arguments;
     //      rescanning needed such that the arguments specified at command line can 
     //      overwrite those specified in input file;
-    command_line_input_processing( argc, argv, "all" );
+	    bool_energy = true;
 
-    
-    if ( c_energy == 'N' ) bool_energy = false;
-    if ( c_energy == 'Y' ) bool_energy = true;
-    assert( bool_energy == true );    
 
     ns_input_derived_variables();
     checking_and_printouts_input_parameters();
 
-    // [2023/07/12] 
-    // NOTE: "file_input_processing ()" and "checking_and_printouts_input_parameters ()" 
-    //       are from "input_processing.cpp". Both still use "XY_to_01 ()", which maps 
-    //       'X' to 1 and 'Y' to 0. 
-    //       
-    //       This time around, we should try to get rid of "XY_to_01 ()", particularly
-    //       because in this folder we are focusing on the "correctness" of numerics 
-    //       (at lower precision).
-
-    // --------------------------------------------------- //
-    // --------------- Numerical parameters -------------- //
-    // --------------------------------------------------- //
 
     dx = static_cast<double>( num_dx_soln ) / static_cast<double>( den_dx_soln );
     dt = dt_max * CFL_constant; 
 
-    // dt = 0.002;
-    // we need more comments above on the types
-
-
-    // {   
-    //     // double max_velocity = 2.;
-    //     // Fwd_Specs.dt = 0.625 * ( ( dx / max_velocity ) / sqrt( static_cast<double>( N_dir ) ) );
-    //     // Fwd_Specs.dt = 1e-4;
-    // }
-
-
-    // -------------------------------------------------- //
-    // --------------- Define the classes --------------- //
-    // -------------------------------------------------- //
 
     std::array< Class_Grid , 2<<(N_dir-1) > Grids;    // NOTE: use bit shift to take the power of 2 (only works when base is 2); 
                                                       //       reason - std::pow () promotes int to float.
-
-    Grids[0].set_grid_parameters ( {'N', 'N'} , bool_energy );
-    Grids[3].set_grid_parameters ( {'M', 'M'} , bool_energy );
+    Class_Grid GridNN;
+    Class_Grid GridMM;
+    GridNN.set_grid_parameters ( {'N', 'N'} , bool_energy );
+    GridMM.set_grid_parameters ( {'M', 'M'} , bool_energy );
     
-    Grids[0].set_forward_operators (); 
-    Grids[3].set_forward_operators (); 
+    GridNN.set_forward_operators (); 
+    GridMM.set_forward_operators (); 
 
     cuda_Class_Grid<'N', 'N', 601, 601> grid_SNN;
     cuda_Class_Grid<'M', 'M', 600, 600> grid_SMM;
 
-		grid_SMM.cuda_Class_Grid_initialize ( &Grids.at(3) );
-		grid_SNN.cuda_Class_Grid_initialize ( &Grids.at(0) );
+		grid_SMM.cuda_Class_Grid_initialize ( &GridMM );
+		grid_SNN.cuda_Class_Grid_initialize ( &GridNN );
     // NOTE: copy the parameters from hst to dev to start the simulation on device
     for ( cuda_Class_Grid_Base * cuda_class_grid : {(cuda_Class_Grid_Base *)&grid_SMM, (cuda_Class_Grid_Base *)&grid_SNN} ) 
     {
