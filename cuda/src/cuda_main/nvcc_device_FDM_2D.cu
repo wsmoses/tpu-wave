c1a61613cdcc57823b2d72965351a89272fa1863
William S. Moses <gh@wsmoses.com>
2026-05-14 17:45:58 -0500

    shorten

--- cuda/src/cuda_main/nvcc_device_FDM_2D.cu
+++ cuda/src/cuda_main/nvcc_device_FDM_2D.cu
@@ -1,68 +1,19 @@
 #include "input_processing.hpp"
 
-#include "grid.cuh"
+#include "grid.hpp"
 #include "namespace_device_variable.cuh"  /* probably dummy, only the #include "namespace_type.cuh" inside it is meaningful */
 
 int main(int argc, char* argv[]) 
 {
-    using ns_forward::N_dir;
-    using namespace ns_input;
 
-#ifndef GRIDFLAG
-    std::string input_file_name = "/InputFile.txt";
-#endif
-
-#if GRIDFLAG == 1
-    std::string input_file_name = "/InputFile_1.txt";
-#elif GRIDFLAG == 3
-    std::string input_file_name = "/InputFile_3.txt";
-#elif GRIDFLAG == 5
-    std::string input_file_name = "/InputFile_5.txt";
-#endif
-
-    // processing the command line option to get the medium_name 
-    // (which determines the input file and data to read in)
-    std::string file_name = "../../data/" + medium_name + input_file_name;  // "/InputFile.txt";    
-
-    file_input_processing( file_name );
-	input_file_name = file_name;  // this is so that we can output "input_file_name" at the end 
-
-
-    ns_input_derived_variables();
-
-    // [2023/07/12] 
-    // NOTE: "file_input_processing ()" and "checking_and_printouts_input_parameters ()" 
-    //       are from "input_processing.cpp". Both still use "XY_to_01 ()", which maps 
-    //       'X' to 1 and 'Y' to 0. 
-    //       
-    //       This time around, we should try to get rid of "XY_to_01 ()", particularly
-    //       because in this folder we are focusing on the "correctness" of numerics 
-    //       (at lower precision).
-
-    // --------------------------------------------------- //
-    // --------------- Numerical parameters -------------- //
-    // --------------------------------------------------- //
-
-    dx = static_cast<double>( num_dx_soln ) / static_cast<double>( den_dx_soln );
-    dt = dt_max * CFL_constant; 
-
-    // dt = 0.002;
-    // we need more comments above on the types
-
-
-
-
-    // -------------------------------------------------- //
-    // --------------- Define the classes --------------- //
-    // -------------------------------------------------- //
-
-    if ( bool_energy ) { ns_input::Record_E_k .allocate_memory (Nt); }
-    if ( bool_energy ) { ns_input::Record_E_p0.allocate_memory (Nt); }
-    if ( bool_energy ) { ns_input::Record_E_p1.allocate_memory (Nt); }
+	std::string input_file_name = "/InputFile.txt";
+	std::string file_name = "../../data/homogeneous" + input_file_name;
+	file_input_processing( file_name );
+	ns_input_derived_variables();
 
     Class_Grid Grid;
     constexpr auto Array_Grid_types = ns_forward::define_Array_Grid_types ();
-    Grid.set_grid_parameters ( Array_Grid_types[0] , bool_energy );
+    Grid.set_grid_parameters ( Array_Grid_types[0] , true );
     Grid.set_forward_operators ();
     Grid.define_parameters_energy ();
     Grid.adjust_parameters_energy_periodic ();
