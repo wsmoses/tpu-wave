0b59505709d99bf606183bcc5da21e1048e0a7d4
William S. Moses <gh@wsmoses.com>
2026-05-15 10:49:39 -0500

    less

--- cuda/src/cuda_main/nvcc_device_FDM_2D.cu
+++ cuda/src/cuda_main/nvcc_device_FDM_2D.cu
@@ -16,12 +16,19 @@ int main(int argc, char* argv[])
     params.time_delay = 0.25;
     params.dx = 0.008;
     params.dt = 1.6e-04;
+
+    params.Nx_prmt = 601;
+    params.Ny_prmt = 601;
+    params.Nx_soln = 601;
+    params.Ny_soln = 601;
     
-    ns_input_derived_variables( params );
+    params.inv_prmt_stride_x = 601;
+    params.inv_prmt_stride_y = 1;
+    params.inv_prmt_size = 361201;
+    params.PADDED_inv_prmt_size = 362404;
 
     Class_Grid Grid(params);
-    constexpr auto Array_Grid_types = ns_forward::define_Array_Grid_types ();
-    Grid.set_grid_parameters ( Array_Grid_types[0] , true );
+    Grid.set_grid_parameters ( {'N','N'} , true );
     Grid.set_forward_operators ();
     Grid.define_parameters_energy ();
     Grid.adjust_parameters_energy_periodic ();
