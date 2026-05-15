c9845de526d860ca5a25be8be56905969a5cfc90
William S. Moses <gh@wsmoses.com>
2026-05-15 10:32:20 -0500

    less

--- cuda/src/cpp/grid.hpp
+++ cuda/src/cpp/grid.hpp
@@ -37,24 +37,12 @@ class Class_Grid
         int N_modulo_x = 1<<30;  // Used to map the indices for periodic BC; needed when using OR not using the % operator
         int N_modulo_y = 1<<30;  // Used to map the indices for periodic BC; needed when using OR not using the % operator
 
-        int SL_external = 1<<30;  // SL : stencil length
-        int SL_internal = 1<<30;  // SL : stencil length
-
         // the number of (wave)fields associated with this grid
         int N_soln = -1;  // 2 for NN ; 0 for MM ; 1 for the others
         // the number of parameters associated with this grid
         int N_prmt = -1;  // 2 for NN ; 0 for MM ; 1 for the others
         // the number of parameters associated with this grid
         int N_enrg = -1;  // 2 for NN ; 0 for MM ; 1 for the others
-
-        // solution variables on this grid (they have the same size as this grid)
-        std::vector< run_time_vector<ns_type::host_precision> > Vec_soln;
-
-        // right hand side variables on this grid (they have the same size as this grid)
-        std::vector< run_time_vector<ns_type::host_precision> > Vec_rths;
-
-        // compensation variables on this grid (they have the same size as this grid)
-        std::vector< run_time_vector<ns_type::host_precision> > Vec_cpst;
 
         // grid name (used in src_rcv.cpp to assign to c_source_type and in output_processing.hpp to define output file name)
         std::string grid_name = "U"; 
@@ -111,20 +99,10 @@ class Class_Grid
             if ( bool_energy == false ) { this->N_enrg = 0; }
 
             // allocate space for solution fields
-            if ( this->N_soln > 0 ) { this->Vec_soln     .reserve(this->N_soln); }
             if ( this->N_prmt > 0 ) { this->Vec_prmt     .reserve(this->N_prmt); }
-
-            if ( this->N_soln > 0 ) { this->Vec_rths     .reserve(this->N_soln); }
-            if ( this->N_soln > 0 ) { this->Vec_cpst     .reserve(this->N_soln); }
-
             if ( this->N_enrg > 0 ) { this->Vec_prmt_enrg.reserve(this->N_enrg); }
 
-            for ( int i = 0; i < this->N_soln; i++ ) { this->Vec_soln     .push_back( run_time_vector<ns_type::host_precision> { G_size_x * G_size_y } ); }
             for ( int i = 0; i < this->N_prmt; i++ ) { this->Vec_prmt     .push_back( run_time_vector<ns_type::host_precision> { G_size_x * G_size_y } ); }
-            
-            for ( int i = 0; i < this->N_soln; i++ ) { this->Vec_rths     .push_back( run_time_vector<ns_type::host_precision> { G_size_x * G_size_y } ); }
-            for ( int i = 0; i < this->N_soln; i++ ) { this->Vec_cpst     .push_back( run_time_vector<ns_type::host_precision> { G_size_x * G_size_y } ); }
-
             for ( int i = 0; i < this->N_enrg; i++ ) { this->Vec_prmt_enrg.push_back( run_time_vector< double >                { G_size_x * G_size_y } ); }
 
         } // set_grid_parameters()
@@ -145,12 +123,6 @@ class Class_Grid
 
             for ( int i=0; i<stencil_dt_dx.length; i++ ) 
                 { this->stencil_dt_dx.at(i) = ns_forward::stencil.at(i) * (dt / dx); }
-
-            SL_external = 1<<30;
-            if ( this->G_type_y == 'M' ) { SL_external = 1; };
-            if ( this->G_type_y == 'N' ) { SL_external = 2; };
-
-            SL_internal = ns_forward::length_stencil - SL_external;
 
             this->stride_x = this->G_size_y;
             this->stride_y =              1;
